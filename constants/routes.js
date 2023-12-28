e40b253051520e711f79f208eb450842926be6d6
Kirill Shevtsov <kirwork.em@gmail.com>
2023-12-28 22:08:59 +0200

    location service added

--- constants/routes.js
+++ constants/routes.js
@@ -3,10 +3,10 @@ const ROUTES = [
         url: '/customers',
         auth: false,
         creditCheck: false,
-        rateLimit: {
-            windowMs: 15 * 60 * 1000,
-            max: 5,
-        },
+        // rateLimit: {
+        //     windowMs: 15 * 60 * 1000,
+        //     max: 5,
+        // },
         proxy: {
             target: 'http://localhost:16011',
             changeOrigin: true,
@@ -21,6 +21,18 @@ const ROUTES = [
         creditCheck: true,
         proxy: {
             target: 'http://localhost:16010',
+            changeOrigin: true,
+            // pathRewrite: {
+            //     [`^/repairers/`]: '',
+            // },
+        },
+    },
+    {
+        url: '/location-api',
+        auth: false,
+        creditCheck: false,
+        proxy: {
+            target: 'http://localhost:16012',
             changeOrigin: true,
             // pathRewrite: {
             //     [`^/repairers/`]: '',
